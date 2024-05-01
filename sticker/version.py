4e2b2f9d23c0b77ffa9bc06e196314e2438cf3b2
Travis Kern <tkern@localhost.localdomain>
2024-05-01 01:59:43 -0400

    Update pack

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '43ad97a3'
+version = '0.1.0+dev.43ad97a3'
+linkified_version = '0.1.0+dev.[43ad97a3](https://github.com/maunium/stickerpicker/commit/43ad97a39da225ef6dcaf1d1054234969a26249d)'
